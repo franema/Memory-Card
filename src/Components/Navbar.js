dd488606d87fb0fb86a456e5e911a328d1d76c70
DonCosme <109149406+franema@users.noreply.github.com>
2022-11-29 10:26:11 -0300

    Adds points counter and title
    Allows the user to know the points earned and gives a brief explanation of the game rules, to improve UX

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+function Navbar({ points }) {
+    return (
+        <div className='navbar_container'>
+            <div className='title_container'>
+                <h2>Memotest!</h2>
+                <p>Get points by clicking on an image but don't click on any more than once!</p>
+            </div>
+            <div className='points_container'>
+                <p>Points: {points}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
